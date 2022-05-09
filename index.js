59a07729bfa27a8c7ddcaf63aa73a919d899fab9
Ilea Beddow <beddow.ilea@gmail.com>
2022-05-09 11:39:56 -1000

    commit tree 6ac8d912d2bb1c9dfda4274012be9f37cc9212b1

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solutions in this file
+
+const names=["Guadalupe", "Ollie", "Aki"]
+const event="surprise" 
+function writeCards(names) {
+    let messages = []
+    for (let i = 0; i < names.length; i++) {
+          messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    
+    }
+    return messages;
+}
+writeCards(messages)
+
+function countDown (positiveNumber){
+    while(positiveNumber >= 0) {
+        console.log(positiveNumber);
+        positiveNumber--;
+
+    }
+}
+console.log(countDown(10))
